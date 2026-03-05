795c2a460cc138c58e33ca925bfacd17bbb6b649
alex-chae-kim <alexk2k05@gmail.com>
2026-03-05 11:18:55 -0500

    Add FIFO and input validation checks for k and m

--- src/cacheSim.cpp
+++ src/cacheSim.cpp
@@ -1,4 +1,6 @@
 # include <iostream>
+# include <vector>
+# include <unordered_set>
 using namespace std;
 
 void test(int k, int m, vector<int> &requests) {
@@ -8,13 +10,46 @@ void test(int k, int m, vector<int> &requests) {
     return;
 }
 
+int FIFO(int k, int m, vector<int> &requests) {
+    int misses = 0;
+    unordered_set<int> cache;
+    queue<int> q;
+    for (auto r : requests) {
+        if (cache.find(r) == cache.end()) {
+            if (cache.size() >= k) {
+                int evict = q.front();
+                q.pop();
+                cache.erase(evict);
+            }
+            cache.insert(r);
+            q.push(r);
+            misses++;
+        }
+    }
+    return misses;
+}
+
 int main() {
     int k, m;
     cin >> k >> m;
+    
+    bool earlyExit = false;
+    if (k < 1) {
+        cout << "Please input k >= 1" << endl;
+        earlyExit = true;
+    }
+    if (m < 1) {
+        cout << "Please input m >= 1" << endl;
+        earlyExit = true;
+    }
+    if (earlyExit) return 0;
+    
     vector<int> requests(m);
     for (auto &r : requests) cin >> r;
 
-    test(k, m, requests);
+    int fifoMisses = FIFO(k, m, requests);
+
+    printf("FIFO  : %d", fifoMisses);
 
     return 0;
 }
